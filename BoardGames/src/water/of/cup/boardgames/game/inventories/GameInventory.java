2dfea5579af7efcf516dc0429b18de77337c4d59
Matt <gr8bitmail@gmail.com>
2021-04-21 21:52:29 -0500

    Added and tested string building

--- BoardGames/src/water/of/cup/boardgames/game/inventories/GameInventory.java
+++ BoardGames/src/water/of/cup/boardgames/game/inventories/GameInventory.java
@@ -1,6 +1,7 @@
 package water.of.cup.boardgames.game.inventories;
 
 import de.themoep.inventorygui.InventoryGui;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import water.of.cup.boardgames.BoardGames;
@@ -32,15 +33,7 @@ public abstract class GameInventory {
     }
 
     public void build(Player player, GameInventoryCallback callback) {
-        // if idle, build create game
-
-        // save game settings
-
-//        String[] guiSetup = {
-//                "  s i z  ",
-//                "  ggggg  ",
-//                "  fpdnl  "
-//        };
+        // TODO: if idle, build create game
 
         char[][] guiSetup = new char[6][9];
         ArrayList<GameOption> gameOptions = getOptions();
@@ -51,12 +44,15 @@ public abstract class GameInventory {
         }
 
         for(int y = 1; y <= 3; y++) {
-            for(int x = 1; x <= 8; x++) {
+            for(int x = 1; x <= 7; x++) {
                 guiSetup[y][x] = 'w';
             }
         }
 
+        // TODO: figure out what to do with this when game is created
         HashMap<Character, GameOption> characterMap = new HashMap<>();
+
+        // TODO: Maybe set to class variable after created
         HashMap<String, Object> gameData = new HashMap<>();
 
         int[] startPos = new int[] {1, 2};
@@ -67,7 +63,7 @@ public abstract class GameInventory {
             switch (gameOption.getOptionType()) {
                 case TOGGLE: {
                     guiSetup[startPos[1]][startPos[0]] = startChar;
-                    guiSetup[startPos[1] - 1][startPos[0]] = (char) (startChar + 1);
+                    guiSetup[startPos[1] + 1][startPos[0]] = (char) (startChar + 1);
 
                     characterMap.put(startChar, gameOption);
                     characterMap.put((char) (startChar + 1), gameOption);
@@ -76,9 +72,9 @@ public abstract class GameInventory {
                     break;
                 }
                 case COUNT: {
-                    guiSetup[startPos[1] + 1][startPos[0]] = startChar;
+                    guiSetup[startPos[1] - 1][startPos[0]] = startChar;
                     guiSetup[startPos[1]][startPos[0]] = (char) (startChar + 1);
-                    guiSetup[startPos[1] - 1][startPos[0]] = (char) (startChar + 2);
+                    guiSetup[startPos[1] + 1][startPos[0]] = (char) (startChar + 2);
 
                     characterMap.put(startChar, gameOption);
                     characterMap.put((char) (startChar + 1), gameOption);
@@ -88,11 +84,28 @@ public abstract class GameInventory {
                     break;
                 }
             }
-            // increment startPos
 
+            // increment startPos
+            startPos[0] += 2;
         }
 
-//        InventoryGui gui = new InventoryGui(BoardGames.getInstance(), player, game.getGameName(), guiSetup);
+        String[] guiSetupString = new String[6];
+        for(int y = 0; y < guiSetup.length; y++) {
+            StringBuilder row = new StringBuilder();
+            for(int x = 0; x < guiSetup[y].length; x++) {
+                row.append(guiSetup[y][x]);
+            }
+            guiSetupString[y] = row.toString();
+        }
+
+        Bukkit.getLogger().info("[BoardGamesDebug] Gui Setup: ");
+        for(String line : guiSetupString) {
+            Bukkit.getLogger().info("'" + line + "'");
+        }
+
+        // TODO: Parse each icon into buttons
+
+//        InventoryGui gui = new InventoryGui(BoardGames.getInstance(), player, game.getGameName(), guiSetupString);
 
 
 //        getOptions();
