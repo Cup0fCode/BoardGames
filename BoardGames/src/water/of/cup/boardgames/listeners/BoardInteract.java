010d4bb69ebf5ed46df3ce357ae84abe7607bec9
Achoowy <Achoowy@gmail.com>
2021-06-20 21:18:38 -0500

    Games rendered when clicked

--- BoardGames/src/water/of/cup/boardgames/listeners/BoardInteract.java
+++ BoardGames/src/water/of/cup/boardgames/listeners/BoardInteract.java
@@ -176,7 +176,8 @@ public class BoardInteract implements Listener {
 				game.click(player, loc, map);
 			} else {
 				game.displayGameInventory(player);
-				player.sendMessage("You are not part of this game");
+				game.renderInitial();
+				//player.sendMessage("You are not part of this game");
 			}
 
 			e.setCancelled(true);
